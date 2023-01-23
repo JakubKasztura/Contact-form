de3720ec6721bdce6d00822051b4700875d0fc45
Jakub Kasztura <jakub.kasztura@gmail.com>
2023-01-23 21:33:52 +0100

    HTML label tags and inputs validation
    Add html label tags for inputs and textarea to show them where user set wrong inputs. Add them to validation function to check if they appear properly when button is clicked.

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
 const FORM = document.querySelector(".form__body"),
   SUBMIT_BUTTON = document.querySelector(".form__submit");
+const name = document.querySelector(".form__name"),
+  email = document.querySelector(".form__email"),
+  message = document.querySelector(".form__message"),
+  labels = document.querySelectorAll("label");
+// change to CAPTIONS NAME etc
 
 function sendMessage(e) {
   e.preventDefault();
-  const createdDataObject = createDataObj();
+
+  const createdDataObject = createDOMDataObj();
   console.log("clicked");
   console.log(createdDataObject);
 }
-function createDataObj() {
-  const name = document.querySelector(".form__name").value,
-    email = document.querySelector(".form__email").value,
-    message = document.querySelector(".form__message").value;
+function createDOMDataObj() {
   const dataObject = {
-    name: name,
-    email: email,
-    message: message,
+    userName: name,
+    userEmail: email,
+    userMessage: message,
   };
+
   validateInputs(dataObject);
   return dataObject;
 }
 function validateInputs(dataObject) {
   for (const key in dataObject) {
-    if (dataObject[key] === "") {
-      alert(`Please set ${key} value`);
-    } else if (key === "email") {
+    if (dataObject[key].value === "") {
+      console.log("wrong");
+      for (const label of labels) {
+        label.classList.toggle("label--disabled");
+      }
+    }
+    if (key === "email") {
       if (!dataObject[key].includes("@")) {
         console.log("wrong email adress");
       }
     }
   }
+  // create another object for only values loop it
 }
 SUBMIT_BUTTON.addEventListener("click", sendMessage);
